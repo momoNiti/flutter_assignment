9dfb17b9be23e6f7ace47d36f4f7ee0855c0b743
momoNiti <60070041@kmitl.ac.th>
2019-02-15 16:00:50 +0700

    check

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -42,63 +42,59 @@ class HomePageState extends State<HomePage> {
       home: DefaultTabController(
         length: 10,
         child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Home"),
-        ),
-        body: Center(
-          child: _widgetOptions.elementAt(_selectedIndex),
-        ),
-        bottomNavigationBar: Theme(
-          data: Theme.of(context).copyWith(
-              canvasColor: Colors.blue,
-              primaryColor: Colors.white,
-              textTheme: Theme.of(context).textTheme.copyWith(
-                  caption: TextStyle(color: Colors.lightBlueAccent[200]))),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.view_quilt),
-                title: Container(
-                  height: 0.0,
-                ),
-                
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text("Home"),
+            ),
+            body: Center(
+              child: _widgetOptions.elementAt(_selectedIndex),
+            ),
+            bottomNavigationBar: Theme(
+              data: Theme.of(context).copyWith(
+                  canvasColor: Colors.blue,
+                  primaryColor: Colors.white,
+                  textTheme: Theme.of(context).textTheme.copyWith(
+                      caption: TextStyle(color: Colors.lightBlueAccent[200]))),
+              child: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                currentIndex: _selectedIndex,
+                onTap: _onItemTapped,
+                items: <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.view_quilt),
+                    title: Container(
+                      height: 0.0,
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.notifications),
+                    title: Container(
+                      height: 0.0,
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.explore),
+                    title: Container(
+                      height: 0.0,
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    title: Container(
+                      height: 0.0,
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.settings),
+                    title: Container(
+                      height: 0.0,
+                    ),
+                  ),
+                ],
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.notifications),
-                title: Container(
-                  height: 0.0,
-                ),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.explore),
-                title: Container(
-                  height: 0.0,
-                ),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                title: Container(
-                  height: 0.0,
-                ),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                title: Container(
-                  height: 0.0,
-                ),
-              ),
-            ],
-          ),
-        )
-        ),
+            )),
       ),
-      
     );
-    
   }
 
   void _onItemTapped(int index) {
